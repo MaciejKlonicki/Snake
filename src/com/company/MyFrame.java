af25292d28ee06f5c03d2e79676a3b29ce43665e
Maciej Kłonicki <maciek.klonicki@gmail.com>
2021-08-25 11:17:09 +0200

    I added menu panel (button "open game window" only works for now).

--- src/com/company/MyFrame.java
+++ src/com/company/MyFrame.java
@@ -7,9 +7,9 @@ public class MyFrame extends JFrame {
     MyFrame(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.add(new MyPanel());
-        this.setSize(600,600);
         this.setTitle("Snake");
         this.setResizable(false);
+        this.pack();
         this.setLocationRelativeTo(null);
         this.setVisible(true);
     }
